12c1e03e75ae7a192e7cd7b057409f1e4ceb6f0c
Hung Cao <hcao@cao-tech.com>
2026-03-19 09:15:22 -0400

    Initial Commit

---
+++ Fixie/FixieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixieApp.swift
+//  Fixie
+//
+//  Created by Hung Cao on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct FixieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FixieTests/FixieTests.swift
@@ -0,0 +1,17 @@
+//
+//  FixieTests.swift
+//  FixieTests
+//
+//  Created by Hung Cao on 3/19/26.
+//
+
+import Testing
+@testable import Fixie
+
+struct FixieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
